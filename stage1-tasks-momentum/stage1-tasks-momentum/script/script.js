32b410807d96d9eb471b3d43c83ee7e07b85f600
Sacs001 <github.com/Sacs001/Sacs001.git>
2022-08-10 23:44:47 +0300

    task #2 complete

--- stage1-tasks-momentum/stage1-tasks-momentum/script/script.js
+++ stage1-tasks-momentum/stage1-tasks-momentum/script/script.js
@@ -2,8 +2,11 @@
 
 let timerText = document.querySelector(".time");
 let timerDate = document.querySelector('.date');
+let timerTimeOfDay = document.querySelector(".greeting");
+let enterYourName = document.querySelector(".name");
 let currentTime = '';
 let currentDate = '';
+let timeOfDay = ''
 
 
 function timer() {
@@ -13,8 +16,27 @@ function timer() {
     currentDate = date.toLocaleDateString('en-US', options);
 }
 
-setInterval(() => {
+function getTimeOfDay() {
     timer()
+    let hours = currentTime.split(':');
+    if (hours[0] >= 6 && hours[0] <= 12) timeOfDay = "morning";
+    else if (hours[0] > 12 && hours[0] <= 18) timeOfDay = "day";
+    else if (hours[0] > 18 && hours[0] <= 22) timeOfDay = "evening";
+    else timeOfDay = "night";
+}
+
+
+setInterval(() => {
+    timer();
+    getTimeOfDay();
+    console.log(currentTime)
     timerText.textContent = currentTime;
-    timerDate.textContent = currentDate
+    timerDate.textContent = currentDate;
+    timerTimeOfDay.textContent = `Good ${timeOfDay}`;
+
 }, 1000);
+
+enterYourName.addEventListener('change', (event) => {
+    console.log(event)
+    console.log(enterYourName.value)
+})
